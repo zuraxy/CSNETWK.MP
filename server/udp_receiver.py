8363cbf80ffc3fde29d8251058093027fc550b8d
Zuraxy <ZuraxyOfficial@gmail.com>
2025-08-02 22:48:45 +0800

    updated udp_receiver

--- server/udp_receiver.py
+++ server/udp_receiver.py
@@ -3,14 +3,13 @@ import socket
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 50999
-BUFFER_SIZE = 4096
+BUFFER_SIZE = 1024
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
-# sock.bind(('',UDP_PORT))
-print(f"[+] Listening on UDP port {LSNP_PORT}")
+sock.bind((UDP_IP, UDP_PORT)) # should it be sock.bind(('',UDP_PORT)) instead
+print(f"[+] Listening on UDP port {UDP_PORT}")
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    data, addr = sock.recvfrom(BUFFER_SIZE) # buffer size is 1024 bytes
     print("received message: %s" % data)
