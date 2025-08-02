0f0b4ef5d20bc6a3855014976d7aac60eb4219f8
Zuraxy <ZuraxyOfficial@gmail.com>
2025-08-02 22:33:33 +0800

    changed UDP_Port, added buffer size, and added print statement Listening on UDP Port

--- server/udp_receiver.py
+++ server/udp_receiver.py
@@ -2,11 +2,14 @@
 import socket
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 50999
+BUFFER_SIZE = 4096
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
+# sock.bind(('',UDP_PORT))
+print(f"[+] Listening on UDP port {LSNP_PORT}")
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
